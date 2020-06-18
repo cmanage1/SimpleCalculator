df066eae67a867d370d60f332dbd907748ad822c
cmanage1 <chethinlm@gmail.com>
2020-06-17 22:19:03 -0400

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Chethin Manage on 2020-06-17.
+//  Copyright © 2020 Chethin Manage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
